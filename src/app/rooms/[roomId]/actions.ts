2071d8f3b1ae1dc107becf6e670e6766e59487ca
SeifESlimene <s.slimene19@gmail.com>
2024-03-21 05:38:42 +0100

    seventh commit

--- src/app/rooms/[roomId]/actions.ts
+++ src/app/rooms/[roomId]/actions.ts
@@ -12,7 +12,9 @@ export async function generateTokenAction() {
 
   const api_key = process.env.NEXT_PUBLIC_GET_STREAM_API_KEY!;
   const api_secret = process.env.GET_STREAM_SECRET_KEY!;
-  const serverClient = StreamChat.getInstance(api_key, api_secret);
+  const serverClient = StreamChat.getInstance(api_key, api_secret, {
+    timeout: 10000,
+  });
   const token = serverClient.createToken(session.user.id);
   console.log("token", token);
   return token;
